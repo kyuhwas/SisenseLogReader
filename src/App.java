cb459f084e6d1a857971e1e794b9e11dd1ad536a
kbbgll <kgwack@gmail.com>
2018-05-11 22:06:37 +0300

    added datetime verification

--- src/App.java
+++ src/App.java
@@ -9,7 +9,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.DateStringConverter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -21,8 +24,9 @@ public class App extends Application {
     private DatePicker startDatePicker;
     private DatePicker endDatePicker;
     private Button setDatesBtn;
-    private LocalDate startTime;
-    private LocalDate endTime;
+    private TextField startTimeTxtFld;
+    private TextField endTimeTxtFld;
+    public SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
     public static void main(String[] args) {
         launch(args);
@@ -44,46 +48,65 @@ public class App extends Application {
         topMenuContainer.setAlignment(Pos.CENTER);
         topMenuContainer.setPadding(new Insets(10));
 
-        // Labels
+        // Row 1 - Labels
         Label startTimeLabel = new Label("Start");
         topMenuContainer.add(startTimeLabel, 0, 0);
         Label endTimeLabel = new Label("End");
         topMenuContainer.add(endTimeLabel, 1, 0);
 
-        // Buttons
+        // Row 2 - DatePickers  and Submit
         startDatePicker = new DatePicker();
         startDatePicker.setMinSize(50, 10);
-        startDatePicker.setPromptText("MM/DD/YYYY HH:mm:ss");
+        startDatePicker.setPromptText("MM/DD/YYYY");
         topMenuContainer.add(startDatePicker, 0,1);
         endDatePicker = new DatePicker();
         endDatePicker.setMinSize(50, 10);
-        endDatePicker.setPromptText("MM/DD/YYYY HH:mm:ss");
+        endDatePicker.setPromptText("MM/DD/YYYY");
         topMenuContainer.add(endDatePicker, 1,1);
         setDatesBtn = new Button("Submit");
         setDatesBtn.setOnAction(event -> {
 
             try {
 
-                startTime = startDatePicker.getValue();
-                endTime = endDatePicker.getValue();
+                Date startTime = sdt.parse(startDatePicker.getValue() + " " + startTimeTxtFld.getText());
+                Date endTime = sdt.parse(endDatePicker.getValue() + " " + endTimeTxtFld.getText());
 
-                System.out.println(startTime.toString());
-                System.out.println(endTime.toString());
+                if (startTime.after(endTime)){
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid range", ButtonType.OK);
+                    startTimeTxtFld.setText("");
+                    startTime = null;
+                    endTime = null;
+                    endTimeTxtFld.setText("");
+                    alert.showAndWait();
+                }
 
-
+                System.out.println("Start: " + startTime);
+                System.out.println("End: " + endTime);
 
             }
             catch (NullPointerException e){
                 Alert alert = new Alert(Alert.AlertType.ERROR, "Please set both dates", ButtonType.OK);
                 alert.showAndWait();
             }
+            catch (ParseException e){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Incorrect time syntax", ButtonType.OK);
+                alert.showAndWait();
+            }
         });
         topMenuContainer.add(setDatesBtn, 2,1);
 
+        // Row 3 - Time fields
+        startTimeTxtFld = new TextField();
+        startTimeTxtFld.setPromptText("HH:mm");
+        topMenuContainer.add(startTimeTxtFld, 0, 2);
+
+        endTimeTxtFld = new TextField();
+        endTimeTxtFld.setPromptText("HH:mm");
+        topMenuContainer.add(endTimeTxtFld, 1, 2 );
+
+        // UI binding
         rootLayout.setTop(topMenuContainer);
-
         scene = new Scene(rootLayout, 500, 400);
-
         window.setScene(scene);
         window.show();
 
